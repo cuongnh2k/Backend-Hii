6932121a35be639caaf730d7943b7a14388f60a1
Nguyen Huu Cuong <cuongnh2k@gmail.com>
2022-07-21 23:52:44 +0700

    create project

---
+++ src/main/java/com/example/backendhii/BackendHiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendhii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendHiiApplication.class, args);
+    }
+
+}
